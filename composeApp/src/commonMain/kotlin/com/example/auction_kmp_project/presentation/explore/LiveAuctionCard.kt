3e63f0bee45eeedd0ace13ed0428dc822328a2b6
salma.ehab <salma.ehab@expertapps.com.sa>
2025-12-07 22:13:38 +0200

    Refactor: Improve layout flexibility in `LiveAuctionCard`
    - Refactored `BidPart` to accept a `modifier` to allow for more flexible layout compositions.
    - Updated `LiveAuctionCard` to utilize `weight(1f)` on `BidPart` for better space distribution.
    - Adjusted padding and removed fixed widths in `TimeCard` and `LiveAuctionCard` to create a more responsive design.

--- composeApp/src/commonMain/kotlin/com/example/auction_kmp_project/presentation/explore/LiveAuctionCard.kt
+++ composeApp/src/commonMain/kotlin/com/example/auction_kmp_project/presentation/explore/LiveAuctionCard.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -63,16 +64,15 @@ fun LiveAuctionCard() {
                     .padding(16.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Column(modifier = Modifier.width(80.dp)) {
+                Column() {
                     Box(
-                        modifier = Modifier
-                            .fillMaxWidth().height(80.dp)
+                        modifier = Modifier.height(80.dp)
                     ) {
                         Image(
                             painter = painterResource(Res.drawable.car),
                             contentDescription = "car",
                             contentScale = ContentScale.Crop,
-                            modifier = Modifier.fillMaxSize().clip(RoundedCornerShape(10.dp))
+                            modifier = Modifier.size(80.dp).clip(RoundedCornerShape(10.dp))
 
                         )
 
@@ -91,7 +91,7 @@ fun LiveAuctionCard() {
                     )
 
                 }
-                BidPart()
+                BidPart(modifier = Modifier.weight(1f))
 
 
             }
