e68d56a8210157141883a76f0c7cfdea80b57040
ahmed.saied <ahmed.saied@expertapps.com.sa>
2025-12-07 11:58:23 +0200

    add card

---
+++ composeApp/src/commonMain/kotlin/com/example/auction_kmp_project/presentation/explore/AuctionCard.kt
@@ -0,0 +1,14 @@
+package com.example.auction_kmp_project.presentation.explore
+
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview(showBackground = true)
+fun AuctionCard(){
+    Card(
+
+    ) {  }
+
+}
