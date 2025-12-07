2d2d3ee9803797084061b3f0aa52189edca19d15
ahmed.saied <ahmed.saied@expertapps.com.sa>
2025-12-07 10:46:02 +0200

    setup bottom nav bar

--- composeApp/src/commonMain/kotlin/com/example/auction_kmp_project/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/auction_kmp_project/App.kt
@@ -15,21 +15,26 @@ import com.example.auction_kmp_project.presentation.navigation.*
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import auctionkmpproject.composeapp.generated.resources.compose_multiplatform
+import com.example.auction_kmp_project.ui.theme.readexProFontFamily
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     val navController = rememberNavController()
-    Scaffold(
-        bottomBar = { BottomNavBar(navController) }
-    ) { innerPadding ->
-        MainNavHost(
-            navController = navController,
-            modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background)
-        )
+    MaterialTheme(
+    ){
+        Scaffold(
+            bottomBar = { BottomNavBar(navController) }
+        ) { innerPadding ->
+            MainNavHost(
+                navController = navController,
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
+            )
+
+        }
     }
 }
